b2067cdab8c620a1303ae11cd88617cc83f546cf
dperezch <davarmando@gmail.com>
2025-09-12 12:15:25 -0300

    optimización Navbar --> carga más rápida de logo, next image

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,17 +1,28 @@
 "use client";
+
 import { Button } from "@/components/ui/button";
 import { Menu } from "lucide-react";
 import Image from "next/image";
 import { useState } from "react";
+// Importa el archivo SVG como un módulo estático
+import logo from "/public/familia.svg";
 
 const Navbar = () => {
   const [mobile, setMobile] = useState(false);
+
   return (
     <>
       <nav className="bg-yellow-400 p-4">
         <div className="container mx-auto flex justify-between items-center">
           <div className="text-2xl font-bold text-sky-900">
-            <Image src="/familia.svg" width={150} height={100} alt="logo" />
+            {/* 1. Prioriza el logo para que se cargue de inmediato */}
+            <Image
+              src={logo}
+              width={150}
+              height={100}
+              alt="logo"
+              priority={true}
+            />
           </div>
           <div className="hidden md:flex space-x-4">
             <a href="#" className="text-sky-900 hover:text-sky-700">
@@ -31,8 +42,6 @@ const Navbar = () => {
             </a>
           </div>
           <div className="flex items-center space-x-4">
-            {/* <ShoppingCart className="text-sky-900" />
-          <User className="text-sky-900" /> */}
             <Button
               variant="outline"
               size="icon"
@@ -41,6 +50,7 @@ const Navbar = () => {
                 setMobile(!mobile);
               }}
             >
+              {/* 2. El ícono se cargará antes debido al "use client" */}
               <Menu className="h-6 w-6" />
             </Button>
           </div>
